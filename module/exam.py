65bf13d96810f404f780af43a3fd40cb87772db3
JAY-YANG1 <anrdl3294@naver.com>
2021-07-08 15:56:52 +0900

    파이썬 모듈

---
+++ module/exam.py
@@ -0,0 +1,16 @@
+# 시험 통과 여부
+
+def exampass(kor, eng, mat):
+    tot = kor + eng + mat
+    avg = tot / 3
+    isPass = '불합격'
+
+    # if kor >= 60 and eng >= 60 and mat >= 60:
+    # avg >= 60:
+    isAll = avg >= 60
+    is40 = kor < 40 or eng < 40 or mat < 40
+    if isAll and not is40:
+        isPass = '합격'
+
+    # 파이썬에서는 함수의 리턴값으로 2개 이상 지정 가능
+    return tot, avg, isPass
